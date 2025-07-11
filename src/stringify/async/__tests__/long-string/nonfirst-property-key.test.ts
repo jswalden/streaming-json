e3f7b63e17388e34c4b81e3d6d1b23f94656b945
Jeff Walden <jwalden@mit.edu>
2025-07-10 17:38:26 -0700

    fix: Make `nonfirst-property-key.test.ts` resilient against arbitrary property ordering choices in stringification.

--- src/stringify/async/__tests__/long-string/nonfirst-property-key.test.ts
+++ src/stringify/async/__tests__/long-string/nonfirst-property-key.test.ts
@@ -1,27 +1,43 @@
 import { expect, test } from "vitest";
-import { fullStreamingResult } from "../fullStreamingResult.js";
 import { Quantum } from "../../../shared.js";
-import { stringifyAsync } from "../../stringifyAsync.js";
+import { stringifyAsync } from "../../../../index.js";
 
 test("very long successive property", async () => {
+  // The tests performed here depend upon implementation characteristics that
+  // are not part of the promised API.
+
   const longString = new Array(2 * Quantum).fill(0).map((_v, index) => String.fromCharCode(index)).join("");
 
   const value = { [longString + "1"]: 17, [longString + "2"]: 42 };
-  const jsonResult = JSON.stringify(value, null, "X");
-
-  await expect(fullStreamingResult(value, "X")).resolves.toBe(jsonResult);
 
   const out: string[] = [];
   await stringifyAsync(value, undefined, "Q", async (s) => {
     out.push(s);
   });
-
   expect(out[0].startsWith('{\nQ"\\u0000\\u0001')).toBe(true);
   expect(out[0].endsWith('"')).toBe(false);
+
   expect(out[2].endsWith('": ')).toBe(true);
-  expect(out[3]).toBe("17");
+
+  const firstLastDigit = out[2][out[2].length - 4];
+  expect(firstLastDigit).toBeOneOf(["1", "2"]);
+
+  if (firstLastDigit === "1")
+    expect(out[3]).toBe("17");
+  else
+    expect(out[3]).toBe("42");
+
   expect(out[4].startsWith(',\nQ"\\u0000\\u0001')).toBe(true);
   expect(out[6].endsWith('": ')).toBe(true);
-  expect(out[7]).toBe("42");
+
+  const secondLastDigit = out[6][out[6].length - 4];
+  if (firstLastDigit === "1") {
+    expect(secondLastDigit).toBe("2");
+    expect(out[7]).toBe("42");
+  } else {
+    expect(secondLastDigit).toBe("1");
+    expect(out[7]).toBe("17");
+  }
+
   expect(out[8]).toBe("\n}");
 });
