ecda43ef5f9fde8ce1ce7bddf2e944d26ef6fc3b
Jeff Walden <jwalden@mit.edu>
2025-07-28 11:52:42 -0400

    chore: Note that `StringSlice`'s limit will be capped to string length.

--- src/stdlib/string.ts
+++ src/stdlib/string.ts
@@ -10,7 +10,8 @@ export const StringFromCharCode = String.fromCharCode;
 
 /**
  * Return a substring of `s` that starts with character `s[start]` and proceeds
- * up to (but does not include) the character at index `limit`.
+ * up to (but does not include) the character at index `limit`.  If this would
+ * exceed the string's length, return a slice ending at the end of the string.
  */
 export function StringSlice(s: string, start: number, limit: number): string {
   return s.slice(start, limit);
