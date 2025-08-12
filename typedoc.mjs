620f8d187838cde9413e3e44af6f5fddb89863b5
Jeff Walden <jwalden@mit.edu>
2025-08-12 10:56:33 -0700

    chore: Update `typedoc` dependency to eliminate spurious warnings about non-link footnotes.

--- typedoc.mjs
+++ typedoc.mjs
@@ -10,13 +10,6 @@ const config = {
    * @param {import("markdown-it").default} parser
    */
   markdownItLoader(parser) {
-    // Typedoc issues warnings for non-link footnotes, so using footnotes for
-    // paragraphs of text will trigger warnings:
-    //
-    // https://github.com/TypeStrong/typedoc/issues/2991
-    //
-    // Not ideal, but the output looks fine, and Github shows the README with
-    // footnotes just fine, so we suck it up.
     parser.use(MarkdownItFootnotePlugin);
   },
 };
